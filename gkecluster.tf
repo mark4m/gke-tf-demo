c438b41c546e1d5a70ee011b6bcaa7dae777ccbc
mark4m <mark4m@gmail.com>
2020-05-04 19:14:26 -0400

    Updated configs

--- gkecluster.tf
+++ gkecluster.tf
@@ -2,5 +2,18 @@ resource "google_container_cluster" "gke-cluster" {
   name               = "my-first-gke-cluster"
   network            = "default"
   location           = "us-central1-a"
+  remove_default_node_pool = true
   initial_node_count = 1
+}
+
+resource "google_container_node_pool" "gke-cluster-nodes" {
+  name = "my-node-pool"
+  location = "us-central1-a"
+  cluster = google_container_cluster.gke-cluster.name
+  node_count = 1
+
+  node_config {
+    preemptible = true
+    machine_type = "n1-standard-2"
+  }
 }
